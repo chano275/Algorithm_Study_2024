913723f8de41419b6d5af8efe8740f76db2bb5f0
euichan <dmlcks1998@naver.com>
2024-10-02 15:30:21 +0900

    주석추가 feat. GPT

--- 1002_숫자고르기/숫자고르기_김의찬_01.py
+++ 1002_숫자고르기/숫자고르기_김의찬_01.py
@@ -1,33 +1,37 @@
-# https://www.acmicpc.net/problem/2668
 import sys
+# 표준 입력을 파일로부터 읽도록 설정
 sys.stdin = open('input_2668.txt','r')
 
-def DFS(store_start,start,visited):
-    global ans
+def DFS(store_start, start, visited):
+    global ans  # 전역 변수 ans를 사용
+    # 시작 노드와 저장된 노드가 같으면 사이클을 발견한 것
     if store_start == start:
-        ans.update(visited)
+        ans.update(visited)  # 방문한 노드를 결과 집합에 추가
         return
+    # 이미 방문한 노드라면 종료
     if start in visited:
         return
-    visited.add(start)
-    DFS(store_start,num[start],visited)
+    visited.add(start)  # 현재 노드를 방문 리스트에 추가
+    # 다음 노드로 DFS 호출
+    DFS(store_start, num[start], visited)
 
+# 입력 받기
+N = int(input())  # 노드의 수
+num = [0] + [int(input()) for _ in range(N)]  # 각 노드가 가리키는 노드 번호를 저장, 1-indexed
 
-N = int(input())
-num = [0] + [int(input()) for _ in range(N)]
-
-ans = set()
-visited = [False] * (N+1)
-for now in range(1,N+1):
-    temp_set = set()
-    if visited[now]:
+ans = set()  # 사이클에 포함된 노드 집합
+visited = [False] * (N + 1)  # 방문한 노드 체크 리스트
+for now in range(1, N + 1):  # 각 노드에 대해
+    temp_set = set()  # 현재 DFS에 사용될 방문 집합
+    if visited[now]:  # 이미 방문한 노드면 건너뛰기
         continue
-    temp_set.add(now)
-    DFS(now,num[now],temp_set)
-    visited[now] = True
+    temp_set.add(now)  # 현재 노드 추가
+    DFS(now, num[now], temp_set)  # DFS 시작
+    visited[now] = True  # 현재 노드 방문 표시
 
-print(len(ans))
-list_ans = list(ans)
-list_ans.sort()
-for i in list_ans:
+# 결과 출력
+print(len(ans))  # 사이클에 포함된 노드의 수 출력
+list_ans = list(ans)  # 집합을 리스트로 변환
+list_ans.sort()  # 정렬
+for i in list_ans:  # 결과 출력
     print(i)
