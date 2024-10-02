c9af08eb71b320fa224b07651e3aa3c2fb29b744
chanhojeong <chano01794@gmail.com>
2024-10-02 15:26:52 +0900

    제출

--- 1002_숫자고르기/숫자고르기_정찬호_01.py
+++ 1002_숫자고르기/숫자고르기_정찬호_01.py
@@ -1,37 +1,55 @@
-# 브루트포스 < 수정 예정 
+def dfs(_start, cur, _visited):
+    start_visited[cur] = True
 
-from itertools import combinations
+    _next = second[cur]
 
+    if _next == _start:  # 시작한 지점으로 돌아오는 사이클 발견
+        cycle_length = len(_visited)
+        _visited = sorted(_visited)
+        if cycle_length in ans_dict:
+            if _visited not in ans_dict[cycle_length]:
+                ans_dict[cycle_length].append(_visited)
+        else:
+            ans_dict[cycle_length] = [_visited]
+        return
+
+    elif _next in _visited:  # 중간에 사이클 발견
+        start_visited[_next] = True
+        target = _visited[_visited.index(_next):]
+        cycle_length = len(target)
+        target = sorted(target)
+        if cycle_length in ans_dict:
+            if target not in ans_dict[cycle_length]:
+                ans_dict[cycle_length].append(target)
+        else:
+            ans_dict[cycle_length] = [target]
+        return
+
+    if start_visited[_next]:
+        return
+
+    _visited.append(_next)
+    dfs(_start, _next, _visited)
+    _visited.pop()
+
+    return
 
 n = int(input())
-first = [i for i in range(1, n+1)]
-next = [int(input()) for _ in range(n)]
+first = [-1] + [i for i in range(1, n + 1)]
+second = [-1] + [int(input()) for _ in range(n)]
+start_visited = [False] * (n+1)
+ans_dict = {}
 
-# print(first, next)
+for i in range(1, n+1):
+    if start_visited[i]:
+        continue
+    dfs(i, i, [i])
 
-first_set = set(first)
-next_set = set(next)
-
-if len(first_set) == len(next_set):
-    print(n)
-else:
-    flag = 0
-    for i in range(len(next_set), 0, -1):
-        test = combinations(first, i)
-
-        for chk in test:
-            temp = set()
-
-            for c in chk:
-                if next[c-1] in temp:break
-                temp.add(next[c-1])
-            
-            if temp == set(chk):
-                # print(temp, chk)
-                print(len(temp))
-                temp_list = sorted(list(temp))
-                for t in temp_list:print(t)
-                flag = 1
-            
-            if flag == 1:break
-        if flag == 1:break
+# 모든 사이클의 숫자를 결과에 포함
+temp = set()
+for cycle_length in ans_dict:
+    for v in ans_dict[cycle_length]:
+        temp.update(v)
+print(len(temp))
+for t in sorted(temp):
+    print(t)
